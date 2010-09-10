a29d8f19f61535cff16b4ab81d264af88abb5acb
peterlind <peter@lind.be>
2010-09-10 14:40:21 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Assume cloned into ~/dotfiles
+ln -s ~/dotfiles/.bash_aliases ~/
+ln -s ~/dotfiles/.bash_profile ~/
+ln -s ~/dotfiles/.bashrc ~/
+ln -s ~/dotfiles/.git-completion.sh ~/
+ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.irbrc ~/
+ln -s ~/dotfiles/.vim ~/
+ln -s ~/dotfiles/.vimrc ~/
+
